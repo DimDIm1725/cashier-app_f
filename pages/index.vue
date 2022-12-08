26b26f0343341428de348c361e4eceff22fc937a
Putu Rele Bhayu Sakti <relebhayu33@gmail.com>
2022-12-08 20:21:36 +0800

    search by category

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row>
-    <v-col cols="8" class="frame-content">
+    <v-col cols="8" class="frame-content px-6">
       <Products />
     </v-col>
     <v-col cols="4" class="frame-content white">
