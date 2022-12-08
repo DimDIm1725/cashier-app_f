927ad989292b6b795f07a679cd6e83d5a154dc6d
DimDim1725 <dimdim1725@gmail.com>
2022-12-08 20:21:36 +0800

    search by category

--- components/Products.vue
+++ components/Products.vue
@@ -1,22 +1,53 @@
 <template>
-  <v-row>
-    <v-col v-for="(product, index) in products" :key="product.id" cols="2">
-      <v-card :title="product.title" :ripple="true">
-        <v-card-action class="img-product">
-          <v-img :src="require(`@/assets/images/products/${product.thumbnail}`)"></v-img>
-        </v-card-action>
-        <v-card-text align="center" class="product-title">
-          {{ product.title }}
-        </v-card-text>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div>
+    <v-row>
+      <v-col cols="10">
+        Search
+      </v-col>
+      <v-col cols="2">
+        <v-menu>
+          <template v-slot:activator="{ on: category }">
+            <v-btn v-on="category" color="primary">
+              Category
+            </v-btn>
+          </template>
+
+          <v-list>
+            <v-list-item-group v-model="categoryId">
+              <v-list-item v-for="(category, index) in categories" :value="category.id" :key="category.id"
+                :disabled="(category.id === categoryId)">
+                <v-list-item-title>{{ category.title }}</v-list-item-title>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-menu>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col v-for="(product, index) in filteredProducts" :key="product.id" cols="2">
+        <v-card :title="product.title" :ripple="true">
+          <v-card-action class="img-product">
+            <v-img :src="require(`@/assets/images/products/${product.thumbnail}`)"></v-img>
+          </v-card-action>
+          <v-card-text align="center" class="product-title">
+            {{ product.title }}
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
 export default ({
   data() {
     return {
+      categoryId: false,
+      categories: [
+        { id: false, title: 'All' },
+        { id: 1, title: 'Smartphone' },
+        { id: 2, title: 'Camera' },
+      ],
       products: [
         { id: 1, title: 'Asus Zenfone', thumbnail: 'asus-zenfone.png', price: 2000000, categoryId: 1 },
         { id: 2, title: 'Canon Eos 700d', thumbnail: 'canon-eos-700d.png', price: 4300000, categoryId: 2 },
@@ -24,6 +55,15 @@ export default ({
         { id: 4, title: 'Iphone 6 Silver', thumbnail: 'iphone-6-silver.png', price: 2500000, categoryId: 1 },
         { id: 5, title: 'Galaxy A3', thumbnail: 'samsung-galaxy-A3.png', price: 3000000, categoryId: 1 },
       ]
+    }
+  },
+  computed: {
+    filteredProducts() {
+      if (this.categoryId) {
+        return this.products.filter(s => s.categoryId === this.categoryId)
+      }
+
+      return this.products
     }
   }
 })
