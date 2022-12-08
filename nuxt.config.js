c412e9a7428fe3c47099bdea9dccbd856d003811
DimDim1725 <dimdim1725@gmail.com>
2022-12-08 16:20:20 +0800

    disable SSR

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  // Disable SSR
+  ssr: false,
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - cashier-app',
