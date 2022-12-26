aac1addc45bda7ca40c3f1fee10f606742937a85
Putu Rele Bhayu Sakti <relebhayu33@gmail.com>
2022-12-26 11:12:43 +0800

    redirect login ke halaman dashboard

---
+++ pages/dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-row>
+      <v-col cols="10" offset="1">
+        <v-card class="mb-2">
+          <v-toolbar color="primary" class="white--text">
+            Dashboard
+          </v-toolbar>
+          <v-card-text>
+            <h2>Hello, Nama.</h2>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
