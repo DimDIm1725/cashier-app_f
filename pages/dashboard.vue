90d9afddb9b726955c5244d5a4b3baf8811fe1c8
DimDim1725 <dimdim1725@gmail.com>
2022-12-27 14:47:26 +0800

    middleware authenticated

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -8,7 +8,6 @@
           </v-toolbar>
           <v-card-text>
             <h2>Hello, Nama.</h2>
-            {{ authenticated }}
           </v-card-text>
         </v-card>
       </v-col>
@@ -17,22 +16,7 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
-
 export default ({
-  computed: {
-    ...mapGetters('auth', {
-      authenticated: 'authenticated'
-    })
-  },
-  mounted() {
-    // cek auth
-    if (!this.authenticated) {
-      this.$router.push({
-        name: 'login___' + this.$i18n.locale,
-        params: { message: 'AUTH_REQUIRED' }
-      })
-    }
-  }
+  middleware: ['authenticated'],
 })
 </script>
