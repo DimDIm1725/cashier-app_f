589a407d7e1c05908ef01188d11ffa05f18a34ee
Putu Rele Bhayu Sakti <relebhayu33@gmail.com>
2022-12-30 10:28:34 +0800

    pages user management

---
+++ pages/users/create.vue
@@ -0,0 +1,3 @@
+<template>
+  <p>Halaman create</p>
+</template>
---
+++ pages/users/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <v-row>
+      <v-col cols="10" offset="1">
+        <v-card class="my-3">
+          <v-toolbar color="primary" class="white--text">
+            Users
+          </v-toolbar>
+          <v-card-text>
+            <div class="mb-4">
+              <v-breadcrumbs class="pa-0" :items="breadcrumbs" />
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default ({
+  data() {
+    return {
+      breadcrumbs: [
+        {
+          text: 'Users',
+          disabled: true,
+          to: '/users'
+        }
+      ]
+    }
+  }
+})
+</script>
