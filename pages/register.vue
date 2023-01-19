136a87a853b55ecaf010cfa9497c11bc5b2b03eb
DimDim1725 <dimdim1725@gmail.com>
2023-01-19 12:16:08 +0800

    v-btn prop loading

--- pages/register.vue
+++ pages/register.vue
@@ -20,9 +20,8 @@
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="primary" @click="onSubmit" :disabled="isDisable">
-              <span v-if="!isDisable">Register</span>
-              <v-progress-circular :value="20" color="primary" indeterminate v-else></v-progress-circular>
+            <v-btn color="primary" @click="onSubmit" :loading="isDisable">
+              Register
             </v-btn>
           </v-card-actions>
         </v-card>
